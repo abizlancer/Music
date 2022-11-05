c24ca600efc76238a822fb053f11f983aeb933ba
abizlancer <abizlancer@gmail.com>
2022-11-05 14:15:27 +0500

    music player

---
+++ src/components/LoginFrom.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="text-white text-center font-bold p-4 rounded mb-4"
+    v-if="log_show_alert"
+    :class="log_alert_variant"
+  >
+    {{ log_alert_msg }}
+  </div>
+  <vee-form
+    :validation-schema="logInSchema"
+    @submit="logIn"
+  >
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <vee-field
+        type="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+        name="email"
+      />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <vee-field
+        type="password"
+        name="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Password"
+      />
+      <ErrorMessage class="text-red-600" name="password" />
+    </div>
+    <button
+      type="submit"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+      :disabled="log_in_submission"
+    >
+      Submit
+    </button>
+  </vee-form>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      logInSchema: {
+        email: "required|email",
+        password: "required|min:9|max:100",
+      },
+      log_in_submission: false,
+      log_show_alert: false,
+      log_alert_variant: "bg-blue-500",
+      log_alert_msg: "Please wait! We are logging you in.",
+    }
+  },
+  methods: {
+    logIn(values) {
+      this.log_in_submission = true
+      this.log_show_alert = true
+      this.log_alert_variant = "bg-blue-500"
+      this.log_alert_msg = "Please wait! We are logging you in."
+      
+      this.log_alert_variant = "bg-green-500"
+      this.log_alert_msg = "Success! You are now logged in."
+      console.log(values);
+    }
+  },
+};
+</script>
