d65b546114f1f0ce021513980abca101a0ca07f4
abizlancer <abizlancer@gmail.com>
2022-11-29 13:37:11 +0500

    feat: add uploading files and progress bar

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCO_ST7vr1L83Qwm0cBdR9un-bJJ6izLBc",
@@ -13,11 +14,13 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
 
 export {
   auth,
   db,
-  usersCollection
+  usersCollection,
+  storage
 }
