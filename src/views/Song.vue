5a9fd9d0eb88f9e249cc6ebc1ff952bf5065690e
abizlancer <abizlancer@gmail.com>
2022-12-10 18:07:34 +0500

    feat: updating the comment count

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -27,7 +27,7 @@
       >
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
           <!-- Comment Count -->
-          <span class="card-title">Comments (15)</span>
+          <span class="card-title">Comments ({{ song.comment_count }})</span>
           <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
         </div>
         <div class="p-6">
@@ -111,6 +111,10 @@ export default {
       this.$router.push({ name: 'home' })
       return
     }
+    
+    const { sort } = this.$route.query
+    this.sort = sort === '1' || sort === '2' ? sort: '1';
+    
     this.song = docSnapshot.data()
     this.getComments()
   },
@@ -130,6 +134,12 @@ export default {
       }
       
       await commentsCollection.add(comment)
+      
+      this.song.comment_count += 1
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count,
+      })
+      
       this.getComments()
       
       this.comment_in_submission = false
@@ -149,5 +159,18 @@ export default {
       })
     }
   },
+  watch: {
+    sort(newVal) {
+      if(newVal === this.$route.query.sort) {
+        return;
+      }
+      
+      this.$router.push({
+        query: {
+          sort: newVal,
+        }
+      })
+    }
+  }
 }
 </script>
